5e7978923c96b72fd2bd6112ce0508bdc61f8ebf
Rayn93 <robert.saternus@gmail.com>
2023-05-17 11:05:42 +0200

    Change module name and add magento/module-store

--- src/registration.php
+++ src/registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Factfinder_ExportModule', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Factfinder_Export', __DIR__);
