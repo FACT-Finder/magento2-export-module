2129ba7a187d25a2aae4b493edcd5669602fc3a0
Rayn93 <robert.saternus@gmail.com>
2023-07-20 14:08:30 +0200

    FFWEB-2758: Add missing test for cms export
    Add missing test for cms export

---
+++ src/Test/Unit/Model/Export/Cms/Field/ContentTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Factfinder\Export\Model\Export\Cms\Field;
+
+use Magento\Email\Model\Template\Filter;
+use PHPUnit\Framework\MockObject\MockObject;
+use Magento\Framework\Model\AbstractModel;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Content
+ */
+class ContentTest extends TestCase
+{
+    /** @var MockObject|Filter */
+    private $filterMock;
+
+    /** @var Content */
+    private $contentField;
+
+    /** @var string */
+    private $content = '<style>some styles</style> Page Content&nbsp;<script type="text/javascript">javascript magic</script>{{}}';
+
+    public function test_replace_unwanted_expressions()
+    {
+        $pageMock = $this->getMockBuilder(AbstractModel::class)
+                         ->disableOriginalConstructor()
+                         ->addMethods(['getContent'])
+                         ->getMock();
+        $pageMock->method('getContent')->willReturn($this->content);
+        $filtered = $this->contentField->getValue($pageMock);
+        $this->assertFalse(strpos($filtered, 'style'), 'Content should be cleared from \<style\> occurrences');
+        $this->assertFalse(strpos($filtered, 'style'), 'Content should be cleared from \<script\> occurrences');
+        $this->assertFalse(strpos($filtered, '&nbsp;'), 'Content should be cleared from @nbsp; occurrences');
+    }
+
+    protected function setUp(): void
+    {
+        $this->filterMock   = $this->createConfiguredMock(Filter::class, ['filter' => $this->content]);
+        $this->contentField = new Content($this->filterMock);
+    }
+}
